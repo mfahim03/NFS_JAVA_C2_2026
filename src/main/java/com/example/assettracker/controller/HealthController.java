2097e24a76261b90d4265ca61a3080efdf02c737
Muhammad Fahim <fahim03@graduate.utm.my>
2026-07-04 12:22:08 +0800

    Exercise 1

--- src/main/java/com/example/assettracker/controller/HealthController.java
+++ src/main/java/com/example/assettracker/controller/HealthController.java
@@ -1,18 +1,29 @@
 package com.example.assettracker.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 
 @RestController // Tells Spring this class is a Controller, Handles HTTP requests
+@RequestMapping("/api")
 public class HealthController {
-    @GetMapping("/api/health")  // maps a GET request to a Java method
+    @GetMapping("/health")  // maps a GET request to a Java method
     public Map<String, String> health() {
         return Map.of(
             "status", "UP",
             "service", "asset-tracker-api"
+        );
+    }
+
+    @GetMapping("/about")
+    public Map<String, String> about() {
+        return Map.of(
+            "appName", "Asset Tracker API",
+            "version", "1.0.0",
+            "description", "API for managing asset tracking"
         );
     }
 }
